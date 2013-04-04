66804bda9d0942544d5ac03019656254e94ee57a
Harshil Mehta <harshil.1990@gmail.com>
2013-04-03 22:24:25 -0400

    added the temp devtalk page

---
+++ application/views/home/devtalk.blade.php
@@ -0,0 +1,25 @@
+@layout('home/application')
+
+@section('content')
+
+<div class="container-extend grey" id="devtalk">
+  <div class="container">
+    <div  class="hero-unit">
+
+      <img id="Image-Maps_1201303101551582" src="/img/Speaker&Networking Poster.jpg" usemap="#Image-Maps_1201303101551582" style="border:2px solid blue;margin-left:100px;" alt="" />
+			<map id="_Image-Maps_1201303101551582" name="Image-Maps_1201303101551582">
+			<area shape="rect" coords="301,413,492,439" href="http://dft.ba/-UofTDev" alt="" title="" target="_blank"   />
+			</map>
+			<!-- Image map text links - Start - If you do not wish to have text links under your image map, you can move or delete this DIV -->
+			<div style="text-align:center; font-size:12px; font-family:verdana; margin-left:auto; margin-right:auto; width:638px;">
+					<a href="http://dft.ba/-UofTDev"></a>
+			</div>
+			<img src="/img/devSpeakers.png" style="border:2px solid blue;margin-left:100px;"/><p>&nbsp</p>
+      <p>Join us for our first event and listen to four engaging speakers from different backgrounds talk about what it's like to be involved in today's tech industry. Then stay for a networking session with the speakers and some of Toronto's top companies, while enjoying food and refreshments.</p>
+						
+    </div>
+  </div>
+</div>
+
+@endsection
+
