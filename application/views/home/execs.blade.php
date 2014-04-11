f3f4caca9f987fcd52b93b1dc53f8354d226d11a
hhaider <hosseinhaider@gmail.com>
2014-04-10 23:53:48 -0400

    Update execs

--- application/views/home/execs.blade.php
+++ application/views/home/execs.blade.php
@@ -7,43 +7,9 @@
 
         <div class="team-list">
             <div class="team-list-item">
-                <div class="team-photo"></div>
-                <div class="team-description">
-                    <h3>President & Co-founder - Emad Alaydi</h3>
-
-                    <p>Emad is an undergraduate student at the University of Toronto (expected: 2014 + PEY) majoring
-                        in Electrical and Computer Engineering and minoring in Engineering Business. His main interests
-                        are in the fields of Power, Telecommunications, Software Development, Computer Networks,
-                        Digital Electronics, Semi-Conductors and management.</p>
-                </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"></div>
-                <div class="team-description">
-                    <h3>VP & Co-founder - Mahmoud Kochaji</h3>
-
-                    <p>Mahmoud is an Electrical and Computer Engineering student at the University of Toronto,
-                        currently doing his PEY as a software developer intern. His main interests are in the fields
-                        of Software, specifically app development and firmware, and Control systems.</p>
-                </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"></div>
-                <div class="team-description">
-                    <h3>VP Strategy - Ahmed Badr</h3>
-
-                    <p>Ahmed Badr received a B.Sc. degree in Electrical Engineering from Cairo University, Egypt in
-                        2007 and M.Sc. degree in Electrical Engineering from Nile University, Egypt in 2009. In
-                        September 2009, he assumed his current position
-                        as a Research Assistant at Signals Multimedia and Security Laboratory in University of Toronto
-                        while pursuing his Ph.D. degree in Electrical Engineering. His research interests include
-                        information theory, coding theory and wireless communications.</p>
-                </div>
-            </div>
-            <div class="team-list-item">
                 <div class="team-photo"><img src="/img/Kristoffer.jpg" alt=""></div>
                 <div class="team-description">
-                    <h3>VP Finance - Kristoffer Flores</h3>
+                    <h3>President - Kristoffer Flores</h3>
 
                     <p>Kris is currently studying Mechanical Engineering at the University of Toronto (1T4). He is
                         enrolled in the Mechatronics and Bioengineering options as well as minoring in the latter.
@@ -53,116 +19,32 @@
                 </div>
             </div>
             <div class="team-list-item">
-                <div class="team-photo"><img src="/img/Valentin.jpg" alt=""></div>
+                <div class="team-photo"></div>
                 <div class="team-description">
-                    <h3>VP Marketing - Valentin Staykov</h3>
+                    <h3>VP - Mehran Hydary</h3>
 
-                    <p>I’m Valentin, a fourth year commerce undergrad student at Rotman Commerce,
-                        specializing in Finance and Economics. I’m currently doing my PEY as a Financial
-                        Analyst at IBM. I’ve always been interested in technology, and just recently started
-                        learning Web development. Something in particular that is of interest to me is integrating
-                        technology in our daily lives in order to simplify mundane tasks.</p>
+                    <p>Mehran is an Electrical Engineering student at the University of Toronto. He has completed an 
+                        internship at AMD and he is currently working at Hydro One. His main interests are in the field 
+                        of Project Management, Power Systems and Telecommunications. Mehran also enjoys taking long runs 
+                        in the park.</p>
                 </div>
             </div>
             <div class="team-list-item">
-                <div class="team-photo"><img src="/img/Amr.jpg" alt=""></div>
+                <div class="team-photo"></div>
                 <div class="team-description">
-                    <h3>VP Design - Amr Ahmed</h3>
+                    <h3>VP Communications - Candi Kimberly chin-sang</h3>
 
-                    <p>Amr Ahmed is a UofT student in Industrial Engineering, currently doing his Professional
-                        Experience Year. He enjoys working with video and animation and found UofT Dev to be a
-                        cool and unique concept. Amr is heading the design team for UofT Dev this year, and believes
-                        the creative aspect is extremely important in the tech space. Apart from video production,
-                        Amr is also passionate about entrepreneurship and culture. He wants to explore new ways
-                        engineering can be applied and used with people and society.</p>
+                    <p></p>
                 </div>
             </div>
             <div class="team-list-item">
                 <div class="team-photo"><img src="/img/Haneef.jpg" alt=""></div>
                 <div class="team-description">
-                    <h3>VP Web & Co-founder - Haneef Ghanim</h3>
+                    <h3>Web Director - Hossein Haider</h3>
 
-                    <p>Hiya! I'm Haneef, a fourth year Computer Engineering student at the University of Toronto. I'm
-                        currently on PEY working as a web developer at an
-                        awesome company called Wattpad (look it up!). I believe in keeping the fun in creating things,
-                        and looking at novel ways in how technology can be incorporated into our lives. </p>
-                </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"><img src="/img/Iggy.jpg" alt=""></div>
-                <div class="team-description">
-                    <h3>Web Team Lead - Owajigbanam Ogbuluijah</h3>
-
-                    <p>The name is Owajigbanam Ogbuluijah, popularly known as "Iggy" at the university.
-                        I am a software engineer in training at the Faculty of Electrical and Computer Engineering. The
-                        InterWeb is my hobby. I have been studying and developing on it for 3+ years.
-                        One of my goals is to push the creativity of every human past activities that can be handled by
-                        a
-                        computer.</p>
-                </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"></div>
-                <div class="team-description">
-                    <h3>VP Android - Mohannad Abwah</h3>
-
-                    <p>Mohannad is currently a fourth year Electrical and Computer Engineering undergraduate
-                        student enrolled in the Professional Experience Year program at the University of Toronto. He
-                        has over 7 years of experience programming to clients, and has worked in a variety of
-                        languages and platforms such as JavaScript in chatrooms, assembly and Verilog on FPGA's, and
-                        applications for mobile devices. He is currently an Agile Software Engineer working at Xtreme
-                        Labs and has worked on mobile apps that have millions of users.</p>
-                </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"></div>
-                <div class="team-description">
-                    <h3>VP iOS - Tamer Abdulazim</h3>
-
-                    <p>I'm Tamer, a Software Engineer with a strong passion for computer science. I have professional
-                        experience using productive languages such as Java (J2ME, J2SE and J2EE), .NET ( C# and VB
-                        ) Mobile technologies (iOS, Android and Blackberry). Currently
-                        I am wearing a researcher hat where I am working to apply my computer science knowledge in
-                        solving real-life problems in transportation field. </p>
-                </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"><img src="/img/Francis.jpg" alt=""></div>
-                <div class="team-description">
-                    <h3>iOS Team Lead - Francis Lata</h3>
-
-                    <p>I am currently an undergraduate student at the University of Toronto Mississauga campus, pursuing a
-                        specialist degree in computer science with a minor in mathematical sciences.
-                        With regards to iOS development, I have been in the iOS development industry for around 2 and a half years, doing freelance
-                        work.  Something I try to remember is that being creative and understanding all the concepts
-                        needed will allow you to make the next greatest app in the App Store! Cheers.</p>
-                </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"><img src="/img/Mark.jpg" alt=""></div>
-                <div class="team-description">
-                    <h3>UTM Representative - Mark Zaky</h3>
-
-                    <p>Mark is an undergraduate student at the University of Toronto Mississauga, expecting to
-                        graduate in 2013, specializing in computer science. He is mostly interested in database
-                        programming, web design, and especially, software project management. Other than software
-                        development and project management,
-                        Mark is also interested in
-                        traveling, being active, and music. He enjoys kickboxing and competing in sporting events
-                        during his free time. In fact, just last summer, he completed the Tough Mudder Toronto 2012
-                        obstacle course.</p>
-                </div>
-            </div>
-			<div class="team-list-item">
-                <div class="team-photo"><img src="/img/Harshil.jpg" alt=""></div>
-                <div class="team-description">
-                    <h3>Webmaster - Harshil Mehta</h3>
-
-                    <p>I'm Harshil, finishing up my computer science specialist degree at UTM with a commerce major. I
-					have professional work experience at IBM for my 16-month PEY internship as a Software Developer
-					using C/C++ and Perl. Currently I'm interested in web development and also working on my own
-					Android application. I like playing strategy based game, so if you're ever playing Age of Empires
-					let me know.</p>
+                    <p>Hossein is a Computer Engineering student at the University of Toronto. He is currently completing 
+                        PEY at Ontario Teachers' Pension Plan as a Software Developer, mainly working with Java and other 
+                        web technologies. He is interested in Software Development methodologies, and web technologies.</p>
                 </div>
             </div>
         </div>
