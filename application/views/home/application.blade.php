8e17ddef9f3ca799ef7cb92a3bd4eea345ddce9c
kflores772 <kflores@blackberry.com>
2014-04-12 15:11:08 -0400

    Added github link to bottom navi

--- application/views/home/application.blade.php
+++ application/views/home/application.blade.php
@@ -1,59 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link href='http://fonts.googleapis.com/css?family=Lato:100,400,700' rel='stylesheet' type='text/css'>
-	<title>UofT Developers</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="css/application.css" media="screen" />
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,400,700' rel='stylesheet' type='text/css'>
+    <title>UofT Developers</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/application.css" media="screen" />
 </head>
 <body>
 
-	<nav class="navbar navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<a class="brand" href="/">U<span class="of-smaller">of</span>T <span class="dev-box">Dev</span> </a>
-				<ul class="nav">
-					<li <?php echo URI::is('*about*') ? 'class="active"' : '' ?>
-							<?php echo URI::is('*execs*') ? 'class="active"' : '' ?>
-							<?php echo URI::is('*contact*') ? 'class="active"' : '' ?>><a href="#">About</a>
-						<ul class="dropdown-menu">
-							<li style="line-height:30px" <?php echo URI::is('*about*') ? 'class="active"' : '' ?>><a href="/about">Mission</a></li>
-							<li style="line-height:30px" <?php echo URI::is('*execs*') ? 'class="active"' : '' ?>><a href="/execs">Execs</a></li>
-							<li style="line-height:30px" <?php echo URI::is('*contact*') ? 'class="active"' : '' ?>><a href="/contact">Contact</a></li>
-						</ul>
-					</li>
-					<li <?php echo URI::is('*join*') ? 'class="active"' : '' ?>><a href="/join">Join us</a></li>
-					<li <?php echo URI::is('*resources*') ? 'class="active"' : '' ?>><a href="/resources">Resources</a></li>
-					<li <?php echo URI::is('*portfolio*') ? 'class="active"' : '' ?>><a href="/portfolio">Portfolio</a></li>
-					<li <?php echo URI::is('*sponsors*') ? 'class="active"' : '' ?>><a href="/sponsors">Sponsors and Partners</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+    <nav class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="brand" href="/">U<span class="of-smaller">of</span>T <span class="dev-box">Dev</span> </a>
+                <ul class="nav">
+                    <li <?php echo URI::is('*about*') ? 'class="active"' : '' ?>
+                            <?php echo URI::is('*execs*') ? 'class="active"' : '' ?>
+                            <?php echo URI::is('*contact*') ? 'class="active"' : '' ?>><a href="#">About</a>
+                        <ul class="dropdown-menu">
+                            <li style="line-height:30px" <?php echo URI::is('*about*') ? 'class="active"' : '' ?>><a href="/about">Mission</a></li>
+                            <li style="line-height:30px" <?php echo URI::is('*execs*') ? 'class="active"' : '' ?>><a href="/execs">Execs</a></li>
+                            <li style="line-height:30px" <?php echo URI::is('*contact*') ? 'class="active"' : '' ?>><a href="/contact">Contact</a></li>
+                        </ul>
+                    </li>
+                    <li <?php echo URI::is('*join*') ? 'class="active"' : '' ?>><a href="/join">Join us</a></li>
+                    <li <?php echo URI::is('*resources*') ? 'class="active"' : '' ?>><a href="/resources">Resources</a></li>
+                    <li <?php echo URI::is('*portfolio*') ? 'class="active"' : '' ?>><a href="/portfolio">Portfolio</a></li>
+                    <li <?php echo URI::is('*sponsors*') ? 'class="active"' : '' ?>><a href="/sponsors">Sponsors and Partners</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-	<div class="wrapper">
-	<div class="contents">
-		@include('plugins.status')
-		@yield('content')
-	</div>
-	</div>
-	
-	<footer>
-		<div class="container">
-			<a class="dev-logo" href="/">U<span class="of-smaller">of</span>T <span class="dev-box white">Dev</span> </a>
-			<ul class="footer-links">
-				<li><a href="/about">About</a></li>
-				<li><a href="/contact">Contact</a></li>
-				<li><a href="http://www.facebook.com/groups/UofTDev/">Facebook</a></li>
-        <li><a href="https://twitter.com/uoftdev">Twitter</a></li>
-        <li><a href="http://www.youtube.com/uoftdevelopers">Youtube</a></li>
-      </ul>
-		</div>
-	</footer>
+    <div class="wrapper">
+    <div class="contents">
+        @include('plugins.status')
+        @yield('content')
+    </div>
+    </div>
+    
+    <footer>
+        <div class="container">
+            <a class="dev-logo" href="/">U<span class="of-smaller">of</span>T <span class="dev-box white">Dev</span> </a>
+            <ul class="footer-links">
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="http://www.facebook.com/groups/UofTDev/">Facebook</a></li>
+                <li><a href="https://github.com/UofTDev">GitHub</a></li>
+                <li><a href="https://twitter.com/uoftdev">Twitter</a></li>
+                <li><a href="http://www.youtube.com/uoftdevelopers">Youtube</a></li>
+            </ul>
+        </div>
+    </footer>
 
-	<script src="/js/jquery.min.js"></script>
-	<script src="/js/bootstrap.min.js"></script>
-	<script src="/js/application.js"></script>
-
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/application.js"></script>
 </body>
 </html>
--- readme.md
+++ readme.md
@@ -5,8 +5,9 @@ This repo contains the source code for www.uoftdev.ca
 * [Haneef Ghanim](https://github.com/haneefghanim)
 * Harshil Mehta
 * [Hossein Haider](https://github.com/hhaider)
-
+* [Kristoffer Flores](https://github.com/kflores772)
 
 ## Committers
 * [Hossein Haider](https://github.com/hhaider)
 * [Kristoffer Flores](https://github.com/kflores772)
+
