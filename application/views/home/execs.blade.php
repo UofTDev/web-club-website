e845787e1ca5a080dda758eca56130fe5f7ab905
kflores772 <kflores@blackberry.com>
2014-05-15 21:42:51 -0400

    Added former members to Team page

--- application/views/home/execs.blade.php
+++ application/views/home/execs.blade.php
@@ -3,44 +3,69 @@
 @section('content')
 <div class="container-extend grey" id="team">
     <div class="container">
-    <div class="hero-unit">
-        <h2>Executive Team</h2>
+        <div class="hero-unit">
+            <h2>Executive Team - 2014/2015</h2>
 
-        <div class="team-list">
-            <div class="team-list-item">
-                <div class="team-photo"><img src="/img/exec/exec_pres.png" alt=""></div>
-                <div class="team-description">
-                    <h3>President - Kristoffer Flores</h3>
-
-                    <p>Kris is currently studying Mechanical Engineering and minoring in Mechatronics. He is doing an internship at BlackBerry as a Software Developer/Tester using web technologies related to Node.js. He is interested in software, quality improvement and electronics. He enjoys coding, basketball, tennis, and staying up to date with the mobile industry.</p>
+            <div class="team-list">
+                <div class="team-list-item">
+                    <div class="team-photo"><img src="/img/exec/exec_pres.png" alt=""></div>
+                    <div class="team-description">
+                        <h3>President - Kristoffer Flores</h3>
+                        <p>Kris is currently studying Mechanical Engineering and minoring in Mechatronics. He is doing an internship at BlackBerry as a Software Developer/Tester using web technologies related to Node.js. He is interested in software, quality improvement and electronics. He enjoys coding, basketball, tennis, and staying up to date with the mobile industry.</p>
+                    </div>
+                </div>
+                <div class="team-list-item">
+                    <div class="team-photo"><img src="/img/exec/exec_vp.png" alt=""></div>
+                    <div class="team-description">
+                        <h3>Vice President - Mehran Hydary</h3>
+                        <p>Mehran is an Electrical Engineering student at the University of Toronto. He has completed an internship at AMD and he is currently working at Hydro One. His main interests are in the field of Project Management, Power Systems and Telecommunications. Mehran also enjoys taking long runs in the park.</p>
+                    </div>
+                </div>
+                <div class="team-list-item">
+                    <div class="team-photo"><img src="/img/exec/exec_comm.png" alt=""></div>
+                    <div class="team-description">
+                        <h3>VP Communications - Candi Chin-Sang</h3>
+                        <p>Candi is majoring in Criminology Studies and Equity Studies, with a minor in Caribbean Studies. She is the president of CARSSU and and executive for University College Orientation. She is currently developing a volunteer initiative for incoming first year students. She is one of two students selected for an international initiative on sustainability, requiring her to travel to Belize in summer 2014 to work with the locals. She enjoys biking and Muy Thai kickboxing.</p>
+                    </div>
+                </div>
+                <div class="team-list-item">
+                    <div class="team-photo"></div>
+                    <div class="team-description">
+                        <h3>Web Director - Hossein Haider</h3>
+                        <p>Hossein is a Computer Engineering student at the University of Toronto. He is currently completing an internship at Ontario Teachers' Pension Plan as a Software Developer, mainly working with Java and other web technologies. He is interested in Software Development methodologies, and web technologies.</p>
+                    </div>
                 </div>
             </div>
-            <div class="team-list-item">
-                <div class="team-photo"><img src="/img/exec/exec_vp.png" alt=""></div>
-                <div class="team-description">
-                    <h3>Vice President - Mehran Hydary</h3>
 
-                    <p>Mehran is an Electrical Engineering student at the University of Toronto. He has completed an internship at AMD and he is currently working at Hydro One. His main interests are in the field of Project Management, Power Systems and Telecommunications. Mehran also enjoys taking long runs in the park.</p>
+            <br />
+            <h2>Former Executives</h2>
+            <div class="team-list">
+                <div class = "team-description">
+                    <p>President/Co-Founder - Emad Alaydi (2012-2014)</p>
                 </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"><img src="/img/exec/exec_comm.png" alt=""></div>
-                <div class="team-description">
-                    <h3>VP Communications - Candi Chin-Sang</h3>
-
-                    <p>Candi is majoring in Criminology Studies and Equity Studies, with a minor in Caribbean Studies. She is the president of CARSSU and and executive for University College Orientation. She is currently developing a volunteer initiative for incoming first year students. She is one of two students selected for an international initiative on sustainability, requiring her to travel to Belize in summer 2014 to work with the locals. She enjoys biking and Muy Thai kickboxing.</p>
+                <div class = "team-description">
+                    <p>VP/Co-Founder - Mahmoud Kochaji (2012-2014)</p>
                 </div>
-            </div>
-            <div class="team-list-item">
-                <div class="team-photo"></div>
-                <div class="team-description">
-                    <h3>Web Director - Hossein Haider</h3>
-
-                    <p>Hossein is a Computer Engineering student at the University of Toronto. He is currently completing an internship at Ontario Teachers' Pension Plan as a Software Developer, mainly working with Java and other web technologies. He is interested in Software Development methodologies, and web technologies.</p>
+                <div class = "team-description">
+                    <p>VP Strategy - Ahmed Badr (2012-2014)</p>
+                </div>
+                <div class = "team-description">
+                    <p>VP Marketing - Valentin Staykov (2012-2014)</p>
+                </div>
+                <div class = "team-description">
+                    <p>Web Director/Co-Founder - Haneef Ghanim (2012-2013)</p>
+                </div>
+                <div class = "team-description">
+                    <p>iOS Director - Tamer Abdulazim (2012-2013)</p>
+                </div>
+                <div class = "team-description">
+                    <p>iOS Director - Francis Lata (2013-2014)</p>
+                </div>
+                <div class = "team-description">
+                    <p>Design Director - Amr Ahmed  (2012-2013)</p>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </div>
 @endsection
