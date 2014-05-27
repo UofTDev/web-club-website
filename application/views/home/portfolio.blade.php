2a25bd6ef5fa13af9dbcc5195fed32834d5065d8
kflores772 <kflores@blackberry.com>
2014-05-27 16:07:03 -0400

    Updated content of portfolio page

--- application/views/home/portfolio.blade.php
+++ application/views/home/portfolio.blade.php
@@ -6,11 +6,42 @@
     <div class="container">
         <div class="hero-unit">
 
-            <h2>Portfolio</h2>
-            
+            <h2>Ongoing Projects</h2>
             <div class="team-list">
                 <div class="team-list-item">
-
+                    <div class="app-photo"></div>
+                    <div class="team-description">
+                        <h3>Photo Quest</h3>
+                        <p>This app will be in development during summer 2014.</p>
+                        <table class="table table-condensed table-hover">
+                            <tr>
+                                <td><b>Role</b></td>
+                                <td><b>Name</b></td>
+                                <td><b>Contact</b></td>
+                            </tr>
+                            <tr>
+                                <td>Team Lead</td>
+                                <td>Mohannad Abwah</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Team Lead</td>
+                                <td>Hossein Haider</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Team Lead</td>
+                                <td>Kristoffer Flores</td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <h2>Portfolio</h2>
+            <div class="team-list">
+                <div class="team-list-item">
                     <div class="app-photo">
                         <img src="img/ic_launcher-web.png">
                         <div class="download-btn"><a href="https://play.google.com/store/apps/details?id=edu.uoft.logaday"><img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_45.png" /></a>
@@ -18,14 +49,24 @@
                     </div>
                     <div class="team-description">
                         <h3>Log a Day</h3>
-                        <p>ONE-ITS smartphone app aims to integrate many features, or mini apps, that target transportation users including auto, transit and non-motorized travellers. </p>
-                            <p>The app will be able to collect different types of data such as location and car driving data. Also, it will be able to provide relevant information and services to the users such as incident alert, car health, advanced route guidance.</p>
-                        <span class="titles">Team Lead:</span> Tamer Abdulazim<br>
+                        <p>Created with the team at ONE-ITS, this smartphone app aims to integrate many features, or mini apps, that target transportation users including auto, transit and non-motorized travellers. </p>
+                        <p>The app will be able to collect different types of data such as location and car driving data. Also, it will be able to provide relevant information and services to the users such as incident alert, car health, advanced route guidance.</p>
+                        <table class="table table-condensed table-hover">
+                            <tr>
+                                <td><b>Role</b></td>
+                                <td><b>Name</b></td>
+                                <td><b>Contact</b></td>
+                            </tr>
+                            <tr>
+                                <td>Team Lead</td>
+                                <td>Tamer Abdulazim</td>
+                                <td></td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             
                 <div class="team-list-item">
-                    
                     <div class="app-photo">
                         <img src="img/CUTC.jpg">
                         <div class="download-btn"><a href="https://play.google.com/store/apps/details?id=https://play.google.com/store/apps/details?id=ca.cutc"><img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_45.png" /></a>
@@ -33,9 +74,24 @@
                     </div>
                     <div class="team-description">
                         <h3>CUTC Infect App</h3>
-                        <p>The Infect mobile app is a tool for delegates of the conference. It acts as their key to personalizing booths and interactions.</p>
-                        <p>The Infect app contains essential conference information, a personalized schedule, a map of the conference, full twitter integration, the ability to easily swap contact info with other CUTC participants.</p>
-                <span class="titles">Team Leads:</span> Mohannad Abwah, Francis Lata
+                        <p>The Infect mobile app is a tool for delegates of the CUTC conference. It acts as their key to personalizing booths and interactions. The app contains essential conference information, a personalized schedule, a map of the conference, full twitter integration, and the ability to easily swap contact info with other participants.</p>
+                        <table class="table table-condensed table-hover">
+                            <tr>
+                                <td><b>Role</b></td>
+                                <td><b>Name</b></td>
+                                <td><b>Contact</b></td>
+                            </tr>
+                            <tr>
+                                <td>Team Lead</td>
+                                <td>Mohannad Abwah</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Team Lead</td>
+                                <td>Francis Lata</td>
+                                <td></td>
+                            </tr>
+                        </table>
                     </div>
                 </div>		
             </div>
